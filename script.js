b9f799d2d733e58bac993869dd1fadd605e45cee
Paul Gerdes <paul-gerds@gmx.net>
2023-09-14 19:59:20 +0200

    first Try of adding js

---
+++ script.js
@@ -0,0 +1,38 @@
+let plus = false;
+let menus = false;
+let multiply = false;
+let divide = false;
+let firstnumber = 0;
+let secondnumber = 0;
+
+function multi() {
+  console.log("multiply");
+  let plus = false;
+  let menus = false;
+  let multiply = true;
+  let divide = false;
+}
+function divi() {
+  console.log("divide");
+  let plus = false;
+  let menus = false;
+  let multiply = false;
+  let divide = true;
+}
+function plusfunc() {
+  console.log("plus");
+  let plus = true;
+  let menus = false;
+  let multiply = false;
+  let divide = false;
+}
+function menusfunc() {
+  console.log("menus");
+  let plus = false;
+  let menus = true;
+  let multiply = false;
+  let divide = false;
+}
+function result() {
+  console.log("result");
+}
