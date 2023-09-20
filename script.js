2e88d189013fef1babcebd7806d19c445a7f5fc2
Paul Gerdes <paul-gerds@gmx.net>
2023-09-20 20:21:07 +0200

    GEILES UPDATE JUNGS

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-let plus = false;
-let menus = false;
-let multiply = false;
-let divide = false;
-let firstnumber = 0;
-let secondnumber = 0;
+const numberOne = [];
+const numberTwo = [];
+let rechenwert1;
+let rechenwert2;
+let Plus = false;
+let ergebnis;
 
-function multi() {
-  console.log("multiply");
-  let plus = false;
-  let menus = false;
-  let multiply = true;
-  let divide = false;
+// const numberbuttons = [{id:"number1"},{id:"number2"},{id:"number3"},{id:"number4"},{id:"number5"},{id:"number6"},{id:"number7"},{id:"number8"},{id:"number9"},]
+function addNumber(pressedNumber) {
+  if (Plus == true) {
+    numberTwo.push(pressedNumber.target.innerText);
+    console.log("numbertwo" + numberTwo);
+    rechenwert2 = +numberTwo.join("");
+    document.getElementById("result").innerHTML = rechenwert2;
+  } else {
+    numberOne.push(pressedNumber.target.innerText);
+    console.log(pressedNumber.target.innerText);
+    console.log("numberone" + numberOne);
+    rechenwert1 = +numberOne.join("");
+    document.getElementById("result").innerHTML = rechenwert1;
+  }
 }
-function divi() {
-  console.log("divide");
-  let plus = false;
-  let menus = false;
-  let multiply = false;
-  let divide = true;
+for (let i = 0; i <= 9; i++) {
+  document.getElementById("number" + i).addEventListener("click", addNumber);
+  document.getElementById("plusbutton").addEventListener("click", () => {
+    Plus = true;
+  });
 }
-function plusfunc() {
-  console.log("plus");
-  let plus = true;
-  let menus = false;
-  let multiply = false;
-  let divide = false;
-}
-function menusfunc() {
-  console.log("menus");
-  let plus = false;
-  let menus = true;
-  let multiply = false;
-  let divide = false;
-}
-function result() {
-  console.log("result");
-}
+document.getElementById("solutionbutton").addEventListener("click", () => {
+  rechenwert1 = +numberOne.join("");
+  console.log(rechenwert1);
+  rechenwert2 = +numberTwo.join("");
+  console.log(rechenwert2);
+  ergebnis = rechenwert1 + rechenwert2;
+  console.log(ergebnis);
+  document.getElementById("result").innerHTML = ergebnis;
+});
